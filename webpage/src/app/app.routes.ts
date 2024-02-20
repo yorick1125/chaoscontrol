822eb1a01c569e3da3ad34c83f8021c80f633e41
yorick1125 <60619352+yorick1125@users.noreply.github.com>
2024-02-20 09:04:29 -0500

    progress

--- webpage/src/app/app.routes.ts
+++ webpage/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
+import { PlayComponent } from './play/play.component';
 
 export const routes: Routes = [
     { path: 'about', component: AboutComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent }
+    { path: 'register', component: RegisterComponent },
+    { path: 'play', component: PlayComponent }
 ];
 
 @NgModule({
